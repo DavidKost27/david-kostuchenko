205a563f4c230cafb963ab77af98883b2ccf50ac
David Kostuchenko <davidkost1999@gmail.com>
2021-10-13 10:47:02 +0300

    [Projects] Images

--- src/components/Projects/index.jsx
+++ src/components/Projects/index.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./styles.scss";
+import { StaticImage } from "gatsby-plugin-image";
 
 export default function Projects() {
   return (
@@ -7,19 +8,42 @@ export default function Projects() {
       <h2>Projects</h2>
 
       <div className="projects__container">
+        <StaticImage
+          src="../../assets/images/edi_hovalot.png"
+          alt="A dinosaur"
+        />
         <h3>Edi Hovalot</h3>
+        <p>
+          A website for a moving company that specialises in sensetive and
+          expensive equipment packaging and transportation.
+        </p>
       </div>
 
       <div className="projects__container">
+        <StaticImage
+          src="../../assets/images/liza_willow.png"
+          alt="A dinosaur"
+        />
         <h3>Liza Willow</h3>
+        <p>
+          Personal online portfolio showcasing the illustrations of a young and
+          aspiring digital illustrator Liza Willow.
+        </p>
       </div>
 
       <div className="projects__container">
         <h3>Steve Antonioni</h3>
+        <p>
+          A personal brand website of Steve Antonioni, a creator & educator that
+          combines the world of pathography and the personal finance niche.
+        </p>
       </div>
 
       <div className="projects__container">
         <h3>David Kostuchenko</h3>
+        <p>
+          My personal website showcasing my professional and personal projects.
+        </p>
       </div>
     </div>
   );
