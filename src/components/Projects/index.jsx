c3d1ad9576520b176dc464094d578426e17cfca0
David Kostuchenko <davidkost1999@gmail.com>
2021-10-13 12:13:42 +0300

    [Update] images

--- src/components/Projects/index.jsx
+++ src/components/Projects/index.jsx
@@ -10,33 +10,58 @@ export default function Projects() {
       <div className="projects__container">
         <StaticImage
           src="../../assets/images/edi_hovalot.png"
-          alt="A dinosaur"
+          alt="Edi Hovalot moving company."
         />
         <h3>Edi Hovalot</h3>
         <p>
           A website for a moving company that specialises in sensetive and
           expensive equipment packaging and transportation.
         </p>
+
+        <a href="" className="link">
+          Github
+        </a>
+        <a href="" className="link">
+          Demo
+        </a>
       </div>
 
       <div className="projects__container">
         <StaticImage
           src="../../assets/images/liza_willow.png"
-          alt="A dinosaur"
+          alt="Liza Willow's Personal online portfolio."
         />
         <h3>Liza Willow</h3>
         <p>
           Personal online portfolio showcasing the illustrations of a young and
           aspiring digital illustrator Liza Willow.
         </p>
+
+        <a href="" className="link">
+          Github
+        </a>
+        <a href="https://lizawillow.com/" className="link">
+          Demo
+        </a>
       </div>
 
       <div className="projects__container">
+        <StaticImage
+          src="../../assets/images/steve_antonioni.png"
+          alt="Steve Antonioni educator & creator."
+        />
         <h3>Steve Antonioni</h3>
         <p>
           A personal brand website of Steve Antonioni, a creator & educator that
           combines the world of pathography and the personal finance niche.
         </p>
+
+        <a href="" className="link">
+          Github
+        </a>
+        <a href="" className="link">
+          Demo
+        </a>
       </div>
 
       <div className="projects__container">
@@ -44,6 +69,13 @@ export default function Projects() {
         <p>
           My personal website showcasing my professional and personal projects.
         </p>
+
+        <a href="" className="link">
+          Github
+        </a>
+        <a href="" className="link">
+          Demo
+        </a>
       </div>
     </div>
   );
